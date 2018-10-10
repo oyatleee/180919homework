8e954683a0b50ce4b2d337a88a44e4bfa6e8b14e
oyatleee <38005165+oyatleee@users.noreply.github.com>
2018-10-10 12:48:03 +0900

    Update homework.js

--- homework.js
+++ homework.js
@@ -83,10 +83,20 @@ const storeC = {
     size: 100
 }
 
+
 const buyFruit = function(fruit, store) {
     if(!store[fruit]) return alert('해당 상점에는'+fruit+'을 팔지않습니다');
     if(store[fruit].stock ===0) return alert('해당 상점에 충분한 재고가 없습니다.');
     store[fruit].stock -= 1;
     alert(fruit+'을 구매하셨습니다');
+    if (fruit[taste] == 'sour') return alert(fruit+'이 시기때문에 리턴해드렸습니다';
+    store[fruit].stock += 1)
 }
 
+const cheapFruit = function(fruit){
+if(fruit ='apple') return alert ('storA');
+    if(fruit='pear') return alert ('sotreB');
+    if(fruit= 'grape')return alert ('storeA');
+    if(fruit='citrus')return alert ('storeC');
+    alert('해당과일을 팔지 않습니다')
+}
